8fe1274964f8fbc64a0a75e5b362c6b6dbbd11fe
Shatlyk Gurbandurdyyev <shatlykgur@gmail.com>
2021-01-09 11:34:52 +0300

    new commit

---
+++ lib/main.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'İlk Projem',
+      theme: ThemeData(
+        primarySwatch: Colors.cyan,
+      ),
+      home: Sayac(
+        isim: "Bardak",
+      ),
+    );
+  }
+}
+
+class Sayac extends StatefulWidget {
+  final String isim;
+
+  Sayac({this.isim});
+
+  @override
+  _SayacState createState() => _SayacState();
+}
+
+class _SayacState extends State<Sayac> {
+  int sayi = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Text("${widget.isim} sayısı:$sayi"),
+      floatingActionButton: FloatingActionButton(
+        onPressed: (ekle), 
+      ),
+    );
+  }
+
+  void ekle() {
+    setState(() {
+      sayi++;
+      print(sayi);
+    });
+  }
+}
