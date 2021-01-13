613bcba23a452554fdfcf5ab1be566c7ba374119
Shatlyk Gurbandurdyyev <shatlykgur@gmail.com>
2021-01-13 23:11:00 +0300

    Adam

--- lib/main.dart
+++ lib/main.dart
@@ -8,47 +8,16 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Projem',
-      theme: ThemeData(
-        primarySwatch: Colors.purple,
-      ),
-      home: GirisSayfasi(),
+      home: Adam(),
     );
   }
 }
 
-class GirisSayfasi extends StatelessWidget {
+class Adam extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Theme.of(context).primaryColor,
-      body: Center(
-        child: Column(
-          children: [
-            SizedBox(height: 55.0,),
-            FlutterLogo(
-              size: 70.0
-            ),
-            SizedBox(height: 25.0,),
-            Text(
-              "SocialWorld",
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 30.0,
-                fontWeight: FontWeight.bold
-              ),
-            ),
-            SizedBox(height: 35.0,),
-            Container(
-              alignment: Alignment.topRight,
-              width: MediaQuery.of(context).size.width - 70.0,
-              height: 180.0,
-              color: Colors.white,
-              ),
-          ],
-        ),
-      ),
+    return Container(
+      
     );
   }
 }
