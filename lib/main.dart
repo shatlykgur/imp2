e65b9063d61e87ab8a611ec2521b23a209484790
Shatlyk Gurbandurdyyev <shatlykgur@gmail.com>
2021-01-18 23:45:29 +0300

    Flutter demo

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(title: 'Flutter Demo'),
+      home: MyHomePage(title: 'Flutter'),
     );
   }
 }
