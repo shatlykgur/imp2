313d0d55c7b46b15a35a97c4332fd8b5aa2935bc
Shatlyk Gurbandurdyyev <shatlykgur@gmail.com>
2021-02-28 18:00:12 +0300

    scaffold widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:improve/gesture_detector.dart';
+import 'package:improve/scaffold_widget.dart';
 import 'package:improve/state_m.dart';
 import 'complex_layout.dart';
 
@@ -15,7 +16,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.orange,
       ),
-      home: StManagement(),
+      home: MyScaffoldWidget(),
     );
   }
 }
